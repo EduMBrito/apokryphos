1e0e1f43065deecf448908b4a9b376cfd6463fc8
EduMBrito <eduardomsbrito@gmail.com>
2026-05-08 22:46:14 -0300

    fix: força bypass do eslint e typescript na vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Ignora erros de linting estrito durante a compilação na Vercel
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Ignora erros de tipagem estrita (como o uso do 'any') na compilação
   typescript: {
     ignoreBuildErrors: true,
   },
 };
-
 export default nextConfig;
